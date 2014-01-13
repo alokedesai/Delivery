6caf3665dac7403dbd2f72baf9d0d43a389b6179
Aloke Desai <aloke.desai@gmail.com>
2014-01-13 12:32:56 +0530

    added imagefield

--- products/models.py
+++ products/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User, UserManager
 
 # Create your models here.
 
 class Type(models.Model):
   name = models.CharField(max_length=300)
-
   def __unicode__(self):
     return self.name
 class Product(models.Model):
   name = models.CharField(max_length=300)
   description = models.TextField()
   price = models.DecimalField(max_digits=6,decimal_places=2)
+  image = models.ImageField(upload_to="images")
   type = models.ForeignKey(Type)
   def __unicode__(self):  # Python 3: def __str__(self):
         return "%s - %s" % (self.name, self.description)
